f72e5dca55fceb59d591eaff2b3f2dd35138c64a
JazJasmine <JazJasmineDev@gmail.com>
2025-10-03 17:42:48 +0200

    chore: minor clean up in logic

--- src/discord/messageCreate.ts
+++ src/discord/messageCreate.ts
@@ -36,63 +36,71 @@ export default (client: Client) =>
     const discordUserId = msg.author.id;
     const result = link(discordUserId, vrcUserId);
 
-    if (result.success) {
-      if (!result.newLink && result.prevVrcUserId) {
-        if (vrcUserId === result.prevVrcUserId) {
-          msg.react('✅');
-          return;
-        }
+    if (!result.success) {
+      discordLog(
+        client,
+        `Failed to add a VRC user link: ${discordUserId}->${vrcUserId}\n\n${result.reason}\n\n${msg.url}`,
+      );
 
-        logger.info(
-          {
-            discordUserId,
-            prevVrcUserId: result.prevVrcUserId,
-            nextVrcUserId: vrcUserId,
-          },
-          'Updated a users link',
-        );
+      msg.reply({
+        content:
+          '❌ Sorry something went wrong linking your account. If that issue persists, please reach out to a mod.',
+      });
+      return;
+    }
 
-        // Remove old Account, if it fails, just log it
-        try {
-          await removeFromVrcGroup(result.prevVrcUserId);
-          discordLog(
-            client,
-            `Removed user from VRC Group, due to link update:\n${result.prevVrcUserId}\n-> ${vrcUserId}\n\n${msg.url}`,
-          );
-        } catch (err: any) {
-          discordLog(
-            client,
-            `Failed to remove user from VRC Group during link update:\n${result.prevVrcUserId}\n\n${err}\n\n${msg.url}`,
-            0xd63509,
-          );
-        }
-      }
-
-      try {
-        await inviteToVrcGroup(vrcUserId);
-        discordLog(
-          client,
-          `Inviting user to group:\n${vrcUserId}\n\n${msg.url}`,
-        );
-
+    if (!result.newLink && result.prevVrcUserId) {
+      if (vrcUserId === result.prevVrcUserId) {
         msg.react('✅');
-      } catch (err: any) {
-        if (err.message.includes('is already a member of this group')) {
-          msg.react('✅');
-          return;
-        }
-
-        discordLog(
-          client,
-          `Failed to invite user to VRC Group during linking:\n${vrcUserId}\n\n${err}\n\n${msg.url}`,
-          0xd63509,
-        );
-
-        msg.react('⚠️');
-        msg.reply(
-          'Something went wrong trying to invite you to the VRC Group. Please try again or contact a mod',
-        );
         return;
       }
+
+      logger.info(
+        {
+          discordUserId,
+          prevVrcUserId: result.prevVrcUserId,
+          nextVrcUserId: vrcUserId,
+        },
+        'Updated a users link',
+      );
+
+      // Remove old Account, if it fails, just log it
+      try {
+        await removeFromVrcGroup(result.prevVrcUserId);
+        discordLog(
+          client,
+          `Removed user from VRC Group, due to link update:\n${result.prevVrcUserId}\n-> ${vrcUserId}\n\n${msg.url}`,
+        );
+      } catch (err: any) {
+        discordLog(
+          client,
+          `Failed to remove user from VRC Group during link update:\n${result.prevVrcUserId}\n\n${err}\n\n${msg.url}`,
+          0xd63509,
+        );
+      }
+    }
+
+    try {
+      await inviteToVrcGroup(vrcUserId);
+      discordLog(client, `Inviting user to group:\n${vrcUserId}\n\n${msg.url}`);
+
+      msg.react('✅');
+    } catch (err: any) {
+      if (err.message.includes('is already a member of this group')) {
+        msg.react('✅');
+        return;
+      }
+
+      discordLog(
+        client,
+        `Failed to invite user to VRC Group during linking:\n${vrcUserId}\n\n${err}\n\n${msg.url}`,
+        0xd63509,
+      );
+
+      msg.react('⚠️');
+      msg.reply(
+        '⚠️ Something went wrong trying to invite you to the VRC Group. Please try again or contact a mod',
+      );
+      return;
     }
   });
